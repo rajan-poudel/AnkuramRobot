66410b3dab467aa6fdc5c0834f8c84756c62fb2d
Rajan Poudel <117752332+rajan-poudel@users.noreply.github.com>
2024-06-12 11:16:40 +0545

    done

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask , render_template ,request ,redirect ,flash
 import subprocess
+import time
+import pyimgur
+import os
+from werkzeug.utils import secure_filename
+from pymongo import MongoClient
+import re
+from datetime import datetime
+
+
+CLIENT_ID = "34982a924cfeb95"
+im = pyimgur.Imgur(CLIENT_ID)
+
+client = MongoClient('mongodb+srv://rajanpoudelnp:HJjU4sEeDNVzOpgL@rajancluster.rpt2ua9.mongodb.net/?retryWrites=true&w=majority&appName=RajanCluster')
+db = client["thingfinder"]
+collection = db["thingfinder"] 
 
 app = Flask(__name__)
+app.config['Upload_Folder'] = 'C:/CodePlayground/Garbage/Robot-Facials/static/Upload_Folder'
+app.secret_key="supersecretkeydkdjd"
 
 @app.route("/")
 def hello_world():
@@ -13,21 +30,68 @@ def robot():
 
 @app.route('/chat')
 def index():
-    #sudo apt install florence
-    # subprocess.run('florence', shell=True)
     return render_template('index.html')
 
-@app.route('/games')
-def games():
+@app.route('/other')
+def other():
     return render_template('games.html')
+
+@app.route('/virtualtour')
+def virtualtour():
+    return render_template('virtualtour.html')
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
 
 @app.route("/photo")
 def photo():
-    # subprocess.run('rpicam-jpeg --output /static/test.jpg', shell=True)
-    return render_template("photo.html")
+    a = int(time.time())
+    subprocess.run(f'rpicam-jpeg --output static/captures/{a}.jpeg', shell=True)
+    # return render_template("photo.html",photo_name=f'captures/{a}.jpeg')
+    return render_template("photo.html",photo_name=f'captures/test.jpg')
+
+@app.route("/share", methods=["POST"])
+def share():
+    photo_name = request.form.get("photo_name")
+    photo_path = f"./static/{photo_name}"
+    uploaded_image = im.upload_image(photo_path, title="Ankuram Robot - By Rajan Poudel")
+    url = uploaded_image.link
+    return render_template("share.html",url=url)
+
+@app.route("/search")
+def search():
+    return render_template("search.html")
+
+@app.route("/thinggot", methods=['GET', 'POST'])
+def thinggot():
+    if request.method == "POST":
+        phone = request.form.get("phone")
+        thinggot = request.form.get("thinggot")
+        file = request.files['file']
+        file.save(os.path.join(app.config['Upload_Folder'],secure_filename(f"{thinggot}.png")))
+        user_data = {
+    "phone": phone,
+    "thinggot":thinggot,
+    "date": datetime.now()
+}
+        result = collection.insert_one(user_data)
+        flash("Your data is submitted","success")
+        return redirect('/search')
+        
+
+@app.route("/thinglost", methods=['GET', 'POST'])
+def thinglost():
+    if request.method == "POST":
+        phone = request.form.get("phone")
+        thinglost = request.form.get("thinglost")
+        pattern = re.compile(re.escape(thinglost), re.IGNORECASE)
+        results = db["thingfinder"].find({"thinggot": pattern})
+        return render_template('found.html', results=results)  
+    return redirect("/search")
 
 
 if __name__ =="__main__": 
-    # import webbrowser
-    # webbrowser.open_new("http://127.0.0.1:5000")
+    #import webbrowser
+    #webbrowser.open_new("http://127.0.0.1:5000")
     app.run(debug=True)
